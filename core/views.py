c3658b7b4920a4ce45919598fa14076b5f141bd0
Arthur Freitas <51511709+arthurfreitasrocha@users.noreply.github.com>
2021-10-25 14:06:14 -0300

    criação do app core - adição das views

---
+++ core/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context = {
+        'nome_pagina': 'Página Inicial'
+    }
+
+    return render(request, 'index.html', context)
+
+
+def catalogo(request):
+    context = {
+        'nome_pagina': 'Catálogo'
+    }
+
+    return render(request, 'catalogo.html', context)
+
+
+def contato(request):
+    context = {
+        'nome_pagina': 'Fale Conosco'
+    }
+
+    return render(request, 'contato.html', context)
